863201003702fa1ef909d16e9925cacf13984eba
simsa <simsaaav@mail.ru>
2026-05-05 22:01:36 +0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import './styles/_reset.scss';
 import './styles/_global.scss';
 import './styles/_main.scss';
 import Header from './components/layouts/Header.jsx';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import HomePage from "./pages/HomePage.jsx";
+
 function App() {
   return (
     <>
+      <BrowserRouter>
         <Header />
         <main>
-          test
+          <Routes>
+            <Route path="/home" element={<HomePage />} />
+          </Routes>
         </main>
+        </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1> AAAAAAAAAAAAAAAAAAAAA</h1>
+    </div>
+  )
+}
+export default HomePage;
